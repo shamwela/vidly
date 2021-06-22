bb9bce8462f165aa8d92dade168134fc0a41f218
Sha Mwe La <shamwela2002@gmail.com>
2021-06-22 15:07:21 +0630

    16 - Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Navbar from './components/navbar';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import MovieDetails from './components/movieDetails';
+import MovieForm from './components/movieForm';
 import Movies from './components/movies';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
-import NotFound from './components/common/notFound';
+import NotFound from './components/notFound';
 
 function App() {
   return (
@@ -13,12 +13,12 @@ function App() {
       <Navbar />
       <div id="content" className="container">
         <Switch>
-          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
           <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
+          <Redirect exact from="/" to="/movies" />
           <Redirect to="/not-found" />
         </Switch>
       </div>
