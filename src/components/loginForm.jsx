fe2166b7d55c1acb364a08c22a5ecc6ca91bd7ca
Sha Mwe La <shamwela2002@gmail.com>
2021-06-23 16:56:25 +0630

    18 - Extracting a Reusable Form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
+import Form from './common/form';
 import Input from './common/input';
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: {
+    data: {
       username: '',
       password: '',
     },
@@ -16,50 +17,13 @@ class LoginForm extends Component {
     password: Joi.string().required().label('Password'),
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) {
-      errors[item.path[0]] = item.message;
-    }
-    return errors;
-  };
-
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     // Will call the server here
     console.log('Submitted');
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
 
     return (
       <>
@@ -67,14 +31,14 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <Input
             name="username"
-            value={account.username}
+            value={data.username}
             label="Username"
             error={errors.username}
             onChange={this.handleChange}
           />
           <Input
             name="password"
-            value={account.password}
+            value={data.password}
             label="Password"
             error={errors.password}
             onChange={this.handleChange}
