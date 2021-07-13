0ebf098257b035d5361262190d5f0d0ea797571b
Sha Mwe La <shamwela2002@gmail.com>
2021-07-13 17:54:56 +0630

    14 - Refactoring

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Joi from 'joi-browser'
 import Form from './common/form'
-import { login } from '../services/authService'
+import auth from '../services/authService'
 
 class LoginForm extends Form {
   state = {
@@ -17,8 +17,7 @@ class LoginForm extends Form {
   doSubmit = async () => {
     try {
       const { username, password } = this.state.data
-      const { data: jwt } = await login(username, password)
-      localStorage.setItem('token', jwt)
+      await auth.login(username, password)
       window.location = '/' // reload the window
     } catch (error) {
       if (error.response && error.response.status === 400) {
