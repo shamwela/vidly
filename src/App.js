c9528b8e94a108193ce08bb840b0088c19049878
Sha Mwe La <shamwela2002@gmail.com>
2021-07-13 17:31:48 +0630

    13 - Logging out a User

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import Customers from './components/customers'
 import Rentals from './components/rentals'
 import NotFound from './components/notFound'
 import NavBar from './components/navBar'
-import LoginForm from './components/loginForm'
 import RegisterForm from './components/registerForm'
+import LoginForm from './components/loginForm'
+import Logout from './components/logout'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 
@@ -33,6 +34,7 @@ class App extends Component {
           <Switch>
             <Route path='/register' component={RegisterForm} />
             <Route path='/login' component={LoginForm} />
+            <Route path='/logout' component={Logout} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies' component={Movies} />
             <Route path='/customers' component={Customers} />
