6f5d8af2c7c6ce233f09ae6d811de4ed4014ad3c
Sha Mwe La <shamwela2002@gmail.com>
2021-07-15 17:46:04 +0630

    18 - Showing or Hiding Elements based on the User

--- src/App.js
+++ src/App.js
@@ -23,17 +23,21 @@ class App extends Component {
   }
 
   render() {
+    const { user } = this.state
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar user={this.state.user} />
+        <NavBar user={user} />
         <main className='container'>
           <Switch>
             <Route path='/register' component={RegisterForm} />
             <Route path='/login' component={LoginForm} />
             <Route path='/logout' component={Logout} />
             <Route path='/movies/:id' component={MovieForm} />
-            <Route path='/movies' component={Movies} />
+            <Route
+              path='/movies'
+              render={(props) => <Movies {...props} user={user} />}
+            />
             <Route path='/customers' component={Customers} />
             <Route path='/rentals' component={Rentals} />
             <Route path='/not-found' component={NotFound} />
