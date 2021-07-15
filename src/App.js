bafe4dea807aebe03a00b3305dd5740a63ea09b9
Sha Mwe La <shamwela2002@gmail.com>
2021-07-15 18:00:35 +0630

    19 - Protecting Routes

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
 
   render() {
     const { user } = this.state
+
     return (
       <React.Fragment>
         <ToastContainer />
@@ -33,7 +34,13 @@ class App extends Component {
             <Route path='/register' component={RegisterForm} />
             <Route path='/login' component={LoginForm} />
             <Route path='/logout' component={Logout} />
-            <Route path='/movies/:id' component={MovieForm} />
+            <Route
+              path='/movies/:id'
+              render={(props) => {
+                if (!user) return <Redirect to='/login' />
+                return <MovieForm {...props} />
+              }}
+            />
             <Route
               path='/movies'
               render={(props) => <Movies {...props} user={user} />}
