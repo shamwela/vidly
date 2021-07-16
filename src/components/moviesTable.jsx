51aa04ed3114e2791ec1f33420e04b7ae3d2d7f1
Sha Mwe La <shamwela2002@gmail.com>
2021-07-16 17:47:43 +0630

    23 - Hiding the Delete Column

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,39 +1,52 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Table from "./common/table";
-import Like from "./common/like";
+import React, { Component } from 'react'
+import auth from '../services/authService'
+import { Link } from 'react-router-dom'
+import Table from './common/table'
+import Like from './common/like'
 
 class MoviesTable extends Component {
   columns = [
     {
-      path: "title",
-      label: "Title",
-      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      path: 'title',
+      label: 'Title',
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
     },
-    { path: "genre.name", label: "Genre" },
-    { path: "numberInStock", label: "Stock" },
-    { path: "dailyRentalRate", label: "Rate" },
+    { path: 'genre.name', label: 'Genre' },
+    { path: 'numberInStock', label: 'Stock' },
+    { path: 'dailyRentalRate', label: 'Rate' },
     {
-      key: "like",
-      content: movie => (
+      key: 'like',
+      content: (movie) => (
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
-      )
+      ),
     },
-    {
-      key: "delete",
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      )
+    {},
+  ]
+
+  deleteColumn = {
+    key: 'delete',
+    content: (movie) => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className='btn btn-danger btn-sm'
+      >
+        Delete
+      </button>
+    ),
+  }
+
+  constructor() {
+    super()
+    const user = auth.getCurrentUser()
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn)
     }
-  ];
+  }
 
   render() {
-    const { movies, onSort, sortColumn } = this.props;
+    const { movies, onSort, sortColumn } = this.props
 
     return (
       <Table
@@ -42,8 +55,8 @@ class MoviesTable extends Component {
         sortColumn={sortColumn}
         onSort={onSort}
       />
-    );
+    )
   }
 }
 
-export default MoviesTable;
+export default MoviesTable
