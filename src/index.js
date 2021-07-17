105ddf6677a9a43e56ef15a938aa6b427b2a0404
Sha Mwe La <shamwela2002@gmail.com>
2021-07-17 17:19:37 +0630

    2 - Environment Variables

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
+
+console.log('SUPERMAN',process.env.REACT_APP_NAME)
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
-);
-registerServiceWorker();
+  document.getElementById('root')
+)
+registerServiceWorker()
