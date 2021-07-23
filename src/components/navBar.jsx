678bb64ed0b75afd532f15a8e123a218ae37ecbb
Sha Mwe La <shamwela2002@gmail.com>
2021-07-23 21:35:38 +0630

    Fix navBar toggle

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,57 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+} from 'reactstrap'
+import './navBar.css'
 
 const NavBar = ({ user }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <Link className='navbar-brand' to='/'>
-        Vidly
-      </Link>
-      <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarNavAltMarkup'
-        aria-controls='navbarNavAltMarkup'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon' />
-      </button>
-      <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
-        <div className='navbar-nav'>
-          <NavLink className='nav-item nav-link' to='/movies'>
-            Movies
-          </NavLink>
-          <NavLink className='nav-item nav-link' to='/customers'>
-            Customers
-          </NavLink>
-          <NavLink className='nav-item nav-link' to='/rentals'>
-            Rentals
-          </NavLink>
-          {!user && (
-            <>
-              <NavLink className='nav-item nav-link' to='/login'>
-                Login
-              </NavLink>
-              <NavLink className='nav-item nav-link' to='/register'>
-                Register
-              </NavLink>
-            </>
-          )}
-          {user && (
-            <>
-              <NavLink className='nav-item nav-link' to='/profile'>
-                {user.name}
-              </NavLink>
-              <NavLink className='nav-item nav-link' to='/logout'>
-                Logout
-              </NavLink>
-            </>
-          )}
-        </div>
-      </div>
-    </nav>
+    <div>
+      <Navbar color='light' light expand='md'>
+        <Link className='navbar-brand' to='/'>
+          Vidly
+        </Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav id='links' className='mr-auto' navbar>
+            <NavItem>
+              <NavLink to='/movies'>Movies</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/customers'>Customers</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/rentals'>Rentals</NavLink>
+            </NavItem>
+            {!user && (
+              <>
+                <NavItem>
+                  <NavLink to='/login'>Login</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to='/register'>Register</NavLink>
+                </NavItem>
+              </>
+            )}
+            {user && (
+              <>
+                <NavItem>
+                  <NavLink to='/profile'>{user.name}</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to='/logout'>Logout</NavLink>
+                </NavItem>
+              </>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   )
 }
 
